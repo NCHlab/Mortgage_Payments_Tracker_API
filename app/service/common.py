fa204b89033e91b28ef705d90c51f9e978b3cf42
NCHLab <mohamudc1@hotmail.com>
2021-12-30 02:06:17 +0000

    generalising funcs

--- app/service/common.py
+++ app/service/common.py
@@ -1,13 +1,14 @@
 from app.core.utils import (
     get_session,
     log_to_db,
+    parse_single_db_data,
     parse_multi_db_data,
 )
 from app.core.db import get_db
 from flask import abort
 
 
-def user_data_from_table(table_name):
+def get_from_table(table_name):
 
     username = get_session()
 
@@ -25,9 +26,9 @@ def user_data_from_table(table_name):
     return list_of_payments
 
 
-def all_user_data_from_table(table_name):
+def get_all_from_table(table_name):
 
-    # Only using to validate user is authenticated
+    # Using to validate user is authenticated
     get_session()
 
     c = get_db().cursor()
@@ -54,6 +55,61 @@ def insert_to_table(table_name, col_names, placeholder, values, log_list):
     log_msg = {
         "message": f"Values inserted into {table_name} by {username}",
         "values": log_list,
+    }
+
+    log_to_db(log_msg)
+
+
+def delete_from_table(_id, table_name):
+    username = get_session()
+    con = get_db()
+
+    with con:
+        data = con.execute(
+            f"""SELECT * FROM {table_name} WHERE id == :id""", {"id": _id}
+        ).fetchone()
+
+        if not data:
+            abort(404)
+
+        con.execute(f"""DELETE FROM {table_name} WHERE id == :id""", {"id": _id})
+
+    data = parse_single_db_data(data)
+
+    log_list = list(data.items())
+
+    log_msg = {
+        "message": f"Values deleted from {table_name} by {username}",
+        "values": log_list,
+    }
+
+    log_to_db(log_msg)
+
+
+def update_table(_id, table_name, col_names_and_placeholder, values):
+
+    username = get_session()
+    con = get_db()
+    with con:
+        data = con.execute(
+            f"""SELECT * FROM {table_name} WHERE id == :id""", {"id": _id}
+        ).fetchone()
+
+        if not data:
+            abort(404)
+
+        con.execute(
+            f"""UPDATE {table_name} SET {col_names_and_placeholder} WHERE id == :id""",
+            values,
+        )
+
+    data = parse_single_db_data(data)
+    prev_vals = list(data.items())
+
+    log_msg = {
+        "message": f"Values updated in home_improvements by {username}",
+        "prev_values": prev_vals,
+        "new_values": [("user_id", username)] + list(values.items()),
     }
 
     log_to_db(log_msg)
--- app/service/home_improvements_service.py
+++ app/service/home_improvements_service.py
@@ -1,24 +1,19 @@
-from flask import abort
-
-from app.core.utils import (
-    get_session,
-    log_to_db,
-    parse_single_db_data,
-)
-from app.core.db import get_db
+from app.core.utils import get_session
 from app.service.common import (
-    user_data_from_table,
-    all_user_data_from_table,
+    get_from_table,
+    get_all_from_table,
     insert_to_table,
+    delete_from_table,
+    update_table,
 )
 
 
 def single_user_home_improvements():
-    return user_data_from_table("home_improvements")
+    return get_from_table("home_improvements")
 
 
 def all_user_home_improvements():
-    return all_user_data_from_table("home_improvements")
+    return get_all_from_table("home_improvements")
 
 
 def insert_home_improvements(body):
@@ -44,115 +39,30 @@ def insert_home_improvements(body):
     ]
 
     insert_to_table(table_name, col_names, placeholder, values, log_list)
-    return
-
-    con = get_db()
-    with con:
-        con.execute(
-            """INSERT INTO home_improvements 
-        (user_id, paid, chargeable, date, reason) 
-        VALUES 
-        (:user_id, :paid, :chargeable, :date, :reason)""",
-            {
-                "user_id": username,
-                "paid": body["paid"],
-                "chargeable": body["chargeable"],
-                "date": body["date"],
-                "reason": body["reason"],
-            },
-        )
-
-    log_msg = {
-        "message": f"Values inserted into home_improvements by {username}",
-        "values": [
-            username,
-            body["paid"],
-            body["reason"],
-            body["date"],
-            body["chargeable"],
-        ],
-    }
-
-    log_to_db(log_msg)
 
 
 def delete_home_improvements(id):
-    username = get_session()
-    con = get_db()
 
-    with con:
-        data = con.execute(
-            """SELECT * FROM home_improvements WHERE id == :id""", {"id": id}
-        ).fetchone()
+    table_name = "home_improvements"
 
-        if not data:
-            abort(404)
-
-        con.execute("""DELETE FROM home_improvements WHERE id == :id""", {"id": id})
-
-    data = parse_single_db_data(data)
-
-    log_msg = {
-        "message": f"Values Deleted from home_improvements by {username}",
-        "values": [
-            username,
-            data["paid"],
-            data["reason"],
-            data["date"],
-            data["chargeable"],
-        ],
-    }
-
-    log_to_db(log_msg)
+    delete_from_table(id, table_name)
 
 
 def update_home_improvements(body):
 
-    username = get_session()
-    con = get_db()
-    with con:
-        data = con.execute(
-            """SELECT * FROM home_improvements WHERE id == :id""", {"id": body["id"]}
-        ).fetchone()
+    get_session()
 
-        if not data:
-            abort(404)
-
-        con.execute(
-            """UPDATE home_improvements
-        SET 
-        paid = :paid,
-        date = :date,
-        reason = :reason,
-        chargeable = :chargeable
-        WHERE id == :id""",
-            {
-                "paid": body["paid"],
-                "date": body["date"],
-                "reason": body["reason"],
-                "chargeable": body["chargeable"],
-                "id": body["id"],
-            },
-        )
-
-    data = parse_single_db_data(data)
-
-    log_msg = {
-        "message": f"Values updated in home_improvements by {username}",
-        "prev_values": [
-            username,
-            data["paid"],
-            data["reason"],
-            data["date"],
-            data["chargeable"],
-        ],
-        "new_values": [
-            username,
-            body["paid"],
-            body["reason"],
-            body["date"],
-            body["chargeable"],
-        ],
+    _id = body["id"]
+    table_name = "home_improvements"
+    col_names_and_placeholder = (
+        "paid = :paid, date = :date, reason = :reason, chargeable = :chargeable"
+    )
+    values = {
+        "paid": body["paid"],
+        "date": body["date"],
+        "reason": body["reason"],
+        "chargeable": body["chargeable"],
+        "id": body["id"],
     }
 
-    log_to_db(log_msg)
+    update_table(_id, table_name, col_names_and_placeholder, values)
